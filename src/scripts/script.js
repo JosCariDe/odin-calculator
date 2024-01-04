8c73ae2fc6621a6cdd935631199f555a00646b2e
JoseCari <jdnarvaez@unimagdalena.edu.co>
2024-01-04 14:01:24 -0500

    Se agregó la función para dar el resultado de la operación Se agregó como evento al btn de igual, para mostrar en pantalla Solo muestra el resultado si solo se hace una operación al tiempo No se puede ingresar un signo antes que los dos valores Está muy verde, muestra resultado en blanco en muchas cosasiones El btn de del aún no se le ha agregado una función

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -39,4 +39,45 @@ btnValores.forEach( btnElement => {
     })
 })
 
+const btnIgual = document.querySelector('#btn-igual');
+btnIgual.addEventListener('click' , () => {
+    pantallaValues.textContent = calcularPantalla(pantallaValues.textContent);
+})
+
+const operacionesAritmeticas = ['+','-','/','*'];
+
+const calcularPantalla = cadena => {
+
+    const arr = cadena.split('');
+
+    console.log(arr);
+
+    let flag = false;
+
+    valor1 = 0;
+    valor2 = 0;
+    operador = '';
+
+
+    arr.forEach( (x,index, arry) => {
+        if (!operacionesAritmeticas.includes(x)) {
+            if (!flag) {
+                valor1 += x;
+            } else {
+                valor2 += x;
+            }
+            
+        } else {
+            operador = x;
+            flag = true;
+        }
+    })
+
+    return operate( Number(valor1),Number(valor2),operador);
+
+}
+
+console.log(calcularPantalla('16+5'));
+console.log(operate(15,5,'+'));
+
 
